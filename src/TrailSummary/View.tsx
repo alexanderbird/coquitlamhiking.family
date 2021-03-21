c9174970ac4243b54c7756a14d536eb19cf98dff
Alexander Bird <alex@alexanderbird.software>
2021-03-21 14:05:45 -0700

    Add colon after 'View'

--- src/TrailSummary/View.tsx
+++ src/TrailSummary/View.tsx
@@ -2,7 +2,7 @@ import { h } from 'preact';
 
 import { SummaryIcon } from './SummaryIcon';
 
-const ViewTitle = () => 'View';
+const ViewTitle = () => 'View: ';
 
 export const View = ({ label }) => (
   <SummaryIcon IconComponent={ViewTitle} label={label} />
