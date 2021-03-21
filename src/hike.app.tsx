d71d65cb78a7d2614318bfedec9dd31d27bee732
Alexander Bird <alex@alexanderbird.software>
2021-03-21 14:05:24 -0700

    Replace json with structured data on hike page; update trail summary style
    Also update buntzen ridge hike notes

--- src/hike.app.tsx
+++ src/hike.app.tsx
@@ -1,12 +1,20 @@
-import { h, FunctionComponent } from 'preact';
+import { h, FunctionComponent, Fragment } from 'preact';
 
 import { App } from './App';
 import { Map } from './Map';
 import { MapMarker } from './MapMarker';
+import { TrailSummary } from './TrailSummary';
 
 export const HikeApp: FunctionComponent<{ hike: any }> = ({ hike }) => (
   <App title={`${hike.name} | Coquitlam Family Hikes`}>
     <div class='hike__header'><a href="/">Home</a> &gt; <h1>{hike.name}</h1></div>
+    <div>{hike.area} Area</div>
+    <TrailSummary {...hike} />
+    { hike.notes && (<>
+        <h4>Notes</h4>
+        <div dangerouslySetInnerHTML={{__html: hike.notes.replace(/\n\n/g, '<br/>')}} />
+      </>
+    )}
     <Map latitude={hike.trailhead.latitude} longitude={hike.trailhead.longitude} zoom={14}>
       <MapMarker latitude={hike.trailhead.latitude} longitude={hike.trailhead.longitude} title='Trailhead' >
         Trailhead:
@@ -14,8 +22,5 @@ export const HikeApp: FunctionComponent<{ hike: any }> = ({ hike }) => (
         <br/><a href={`https://www.google.com/maps/search/?api=1&query=${hike.trailhead.latitude},${hike.trailhead.longitude}`}>Open in Google Maps</a>
       </MapMarker>
     </Map>
-    <pre><code>
-      { JSON.stringify(hike, null, 2) }
-    </code></pre>
   </App>
 );
