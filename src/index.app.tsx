53e9b253bd38e45c95b4cbdfe84f817984d1d9ad
Alexander Bird <alex@alexanderbird.software>
2021-03-21 12:46:22 -0700

    Replace JSON blobs with structured data for main page

--- src/index.app.tsx
+++ src/index.app.tsx
@@ -13,20 +13,39 @@ export const IndexApp = ({ hikes }) => (
         <MapMarker latitude={hike.trailhead.latitude} longitude={hike.trailhead.longitude} title={hike.name} >
           <div>
             <h3><a href={`./${hike.slug}.html`}>{hike.name}</a></h3>
-            <pre><code>
-              { JSON.stringify(hike, null, 2) }
-            </code></pre>
+            <div>
+              <div>Stroller: {hike.stroller}</div>
+              <div>Hilly: {hike.hilly}</div>
+              <div>View: {hike.view}</div>
+            </div>
           </div>
         </MapMarker>
       )) }
     </Map>
-    { hikes.map(hike => (
-      <div>
-        <h3><a href={`./${hike.slug}.html`}>{hike.name}</a></h3>
-        <pre><code>
-          { JSON.stringify(hike, null, 2) }
-        </code></pre>
-      </div>
-    )) }
+    <table>
+      <thead>
+        <tr>
+          <th>Hike</th>
+          <th>Area</th>
+          <th>Trailhead</th>
+          <th>Stroller</th>
+          <th>Hilly</th>
+          <th>View</th>
+        </tr>
+      </thead>
+      <tbody>
+        { hikes.sort((lhs, rhs) => lhs.area > rhs.area ? 1 : -1).map(hike => (
+          <tr>
+            <td><a href={`./${hike.slug}.html`}>{hike.name}</a></td>
+            <td>{hike.area}</td>
+            <td>{hike.trailhead.name}</td>
+            <td>{hike.stroller}</td>
+            <td>{hike.hilly}</td>
+            <td>{hike.view}</td>
+          </tr>
+        )) }
+        
+      </tbody>
+    </table>
   </App>
 );
