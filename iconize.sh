94e8235c3fb820b5d611c46187bb4dac01b9aa89
Alexander Bird <alex@alexanderbird.software>
2021-03-22 10:29:01 -0700

    Add ./iconize script to transform SVGs into icon components; Add View icons

---
+++ iconize.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+source_svg=$1
+component_name=$2
+
+[ "$source_svg" = "" ] || [ "$component_name" = "" ] && {
+  echo "Usage: $0 path/to/source.svg ComponentName"
+  exit 1
+}
+
+destination_tsx="src/icons/$component_name.tsx"
+
+cp "$source_svg" "$destination_tsx"
+npx --no-install svgo -i "$destination_tsx"
+ex $destination_tsx <<EOF
+1 insert
+import { h } from 'preact';
+export const $component_name = () =>
+.
+xit
+EOF
