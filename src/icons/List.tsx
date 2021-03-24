e48c7168d8e35cf66304ed98f867af030166d1db
Alexander Bird <alex@alexanderbird.software>
2021-03-24 06:52:56 -0700

    Add List and Map icons in preparation for a two-tab view of the main page

---
+++ src/icons/List.tsx
@@ -0,0 +1,3 @@
+import { h } from 'preact';
+export const List = () =>
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64"><rect x="4" y="10" width="12" height="12" rx="2" ry="2"/><rect x="20" y="10" width="40" height="12" rx="2" ry="2"/><rect x="4" y="26" width="12" height="12" rx="2" ry="2"/><rect x="20" y="26" width="40" height="12" rx="2" ry="2"/><rect x="4" y="42" width="12" height="12" rx="2" ry="2"/><rect x="20" y="42" width="40" height="12" rx="2" ry="2"/></svg>
