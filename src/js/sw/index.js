7e9d8df461cbac0fb2dde88b82ba2358f60c8884
Alexander Bird <alex@alexanderbird.software>
2021-04-14 08:21:58 -0700

    Update the hiker.family servie worker to expire the google maps cache
    Without that, Google bails with:
    
    > Google Maps JavaScript API error: NotLoadingAPIFromGoogleMapsError
    > https://developers.google.com/maps/documentation/javascript/error-messages#not-loading-api-from-google-maps-error js:68:128

--- src/js/sw/index.js
+++ src/js/sw/index.js
@@ -3,7 +3,7 @@ import { registerRoute } from 'workbox-routing';
 import { ExpirationPlugin } from 'workbox-expiration';
 import { CacheFirst, NetworkFirst, StaleWhileRevalidate } from 'workbox-strategies';
 import { CacheableResponsePlugin } from 'workbox-cacheable-response';
-
+const ONE_DAY_IN_SECONDS = 24 * 60 * 60;
 
 precacheAndRoute(self.__WB_MANIFEST);
 
@@ -20,12 +20,16 @@ registerRoute(
   }),
 );
 
+
 registerRoute(
   ({url}) => url.origin === 'https://maps.googleapis.com' ||
              url.origin === 'https://maps.gstatic.com',
   new NetworkFirst({
     cacheName: 'google-maps',
     plugins: [
+      new ExpirationPlugin({
+        maxAgeSeconds: ONE_DAY_IN_SECONDS * 2,
+      }),
       new CacheableResponsePlugin({
         statuses: [0, 200]
       })
