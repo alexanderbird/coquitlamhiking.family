ab64b3044b9bcb2fd998fed2311450556ac84e51
Alexander Bird <alex@alexanderbird.software>
2021-04-15 08:37:05 -0700

    Revert "Add experimental network-first precache in the service worker"
    This reverts commit 3c925f141c6cc4104edbd00347cf4174c642547a.

--- src/js/sw/index.js
+++ src/js/sw/index.js
@@ -1,11 +1,11 @@
-import { PrecacheController } from 'workbox-precaching';
+import { precacheAndRoute } from 'workbox-precaching';
 import { registerRoute } from 'workbox-routing';
 import { ExpirationPlugin } from 'workbox-expiration';
 import { CacheFirst, NetworkFirst, StaleWhileRevalidate } from 'workbox-strategies';
 import { CacheableResponsePlugin } from 'workbox-cacheable-response';
 const ONE_DAY_IN_SECONDS = 24 * 60 * 60;
 
-precacheAndRouteNetworkFirst(self.__WB_MANIFEST);
+precacheAndRoute(self.__WB_MANIFEST);
 
 registerRoute(
   ({url}) => url.origin === 'https://images.hiker.family',
@@ -37,30 +37,3 @@ registerRoute(
   }),
 );
 
-
-function precacheAndRouteNetworkFirst(precacheManifest) {
-  console.info({ precacheManifest });
-  // Docs: https://developers.google.com/web/tools/workbox/modules/workbox-precaching#using_precachecontroller_directly
-  const precacheController = new PrecacheController();
-  precacheController.addToCacheList(precacheManifest);
-
-  self.addEventListener('install', (event) => {
-    event.waitUntil(precacheController.install(event));
-  });
-
-  self.addEventListener('activate', (event) => {
-    event.waitUntil(precacheController.activate(event));
-  });
-
-  self.addEventListener('fetch', async (event) => {
-    const cacheKey = precacheController.getCacheKeyForURL(event.request.url);
-    const cache = await caches.open(cacheKey)
-    try {
-      const networkResponse = await fetch(event.request);
-      cache.put(event.request, networkResponse.clone());
-      event.respondWith(networkResponse);
-    } catch(e) {
-      event.respondWith(cache.match(event.request));
-    }
-  });
-}
