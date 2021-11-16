607ca8ee6993941313dc884ec6b4208d14aa98fb
Alexander Bird <alex@alexanderbird.software>
2021-11-15 22:24:39 -0800

    Update CSS so that fieldsets show up one at a time
    When the option-field-set--complete class is added to the fieldset, then the next one shows up. When the last one has option-field-set--complete, then the results show up

--- src/app/FindATrailPage.tsx
+++ src/app/FindATrailPage.tsx
@@ -9,7 +9,7 @@ import { Nav } from './Nav';
 
 const metaTags = {
   title: "🥾 Find A Trail",
-  description: "Choose your season, duration, terrain, and incline to find a hike in Coquitlam",
+  description: "Choose your weather, duration, terrain, and incline to find a hike in Coquitlam",
   image: "https://images.hiker.family/deboville-slough/004.jpg?nf_resize=smartcrop&w=800&h=800",
   path: "/find.html"
 }
@@ -39,40 +39,34 @@ const HikeTile = ({ hikes, slug, attributes, notes, name }: HikeTileProps) => {
   );
 }
 
-/*
- * The problem with the pure CSS option is that it doesn't protect you from dead ends. Instead, we should model
- * the hikes as a directed graph from season to duration to terrain to incline to trail. At each step of the wizard,
- * if there are no trail leaf nodes, then don't show the option. 
- *
- * For layer "Season", count each of the seasons as invalid. Do a BFT. Any season that leads to a trail is considered valid. 
- * Allow the user to select any number of valid seasons, at least 1.
- * press next
- * traverse (depth=1) to the durations. Consider each of the durations invalid. Do a BFT. Any duration that leads to a trail is considered valid. 
- * Allow the user to select any number of valid durations.
- * press next.
- * Repeat until we get to trails.
- */
-
 class AttributeType {
   index: number;
   values: AttributeValue[];
   label: string;
+  id: string;
 
   constructor({ index, values, label }) {
+    this.id = 'attribute-type--' + toID(label);
     Object.assign(this, { index, values, label });
   }
+}
+
+function toID(text: string) {
+  return text.toLowerCase().replace(/[^a-z0-9]/g, '');
 }
 
 class AttributeValue {
   code: string;
   label: string;
+  id: string;
   constructor(code, label) {
+    this.id = 'attribute-value--' + toID(label) + '--' + code;
     Object.assign(this, { code, label });
   }
 }
 
 const options = [
-  new AttributeType({ index: 0, label: 'Season', values: [
+  new AttributeType({ index: 0, label: 'Weather', values: [
     new AttributeValue('h', 'Very Hot'),
     new AttributeValue('f', 'Very Foggy'),
     new AttributeValue('e', 'Everything Else')
@@ -96,15 +90,20 @@ const options = [
 
 const OptionFieldSet = ({ option }: { option: AttributeType }) => {
   return (
-    <fieldset data-index={option.index}>
+    <fieldset class='option-field-set' id={option.id} data-index={option.index}>
       <legend>{option.label}</legend>
       { option.values.map(optionValue => <OptionInput value={optionValue} />) }
+      <button class='option-field-set__button-previous'>Previous</button>
+      <button class='option-field-set__button-next'>Next</button>
     </fieldset>
   );
 }
 
 const OptionInput = ({ value }: { value: AttributeValue }) => {
-  return <Checkbox name=''>{value.label}</Checkbox>
+  return (<>
+    <input type='checkbox' id={value.id} />
+    <label for={value.id}>{ value.label }</label>
+  </>);
 }
 
 export const FindATrailPage = ({ hikes }) => (
@@ -119,35 +118,37 @@ export const FindATrailPage = ({ hikes }) => (
           options.map(option => <OptionFieldSet option={option} />)
         }
 
-        <HikeTile hikes={hikes} slug='galette-ave-coquitlam-river'
-          attributes={['season-any', 'duration-30', 'terrain-small', 'incline-flat']} />
-        <HikeTile hikes={hikes} slug='rocky-point-pier' 
-          attributes={['season-any', 'duration-30', 'terrain-smooth', 'incline-flat']} />
-        <HikeTile hikes={hikes} slug='minnekhada' notes='Fern Trail, Lodge Trail&comma; or Addington Lookout Trail'
-          attributes={['season-any', 'duration-30', 'terrain-small', 'incline-ups-and-downs']} />
-        <HikeTile hikes={hikes} slug='harper-park' 
-          attributes={['season-any', 'duration-30', 'terrain-smooth', 'incline-hilly']} />
-        <HikeTile hikes={hikes} slug='pitt-river' notes="for a loop, try the Deboville Slough. If it's quite windy, consider Colony Farm Regional Park instead."
-          attributes={['season-any', 'duration-60', 'terrain-smooth', 'incline-flat']} />
-        <HikeTile hikes={hikes} slug='admiralty-point' 
-          notes="option to turn back at Cod Rock or Maple Beach"
-          attributes={['season-any', 'duration-60', 'terrain-large', 'incline-ups-and-downs']} />
-        <HikeTile hikes={hikes} slug='deiner-creek'
-          attributes={['season-summer', 'duration-60', 'terrain-large', 'incline-hilly']} />
-        <HikeTile hikes={hikes} slug='pinecone-burke' 
-          name="Pinecone Burke: Frank's to Hustler via Conifer"
-          notes="Frank's &rarr; Conifer Drive &rarr; Hustler"
-          attributes={['season-any', 'duration-60', 'terrain-large', 'incline-hilly']} />
-        <HikeTile hikes={hikes} slug='pinecone-burke' 
-          name="Pinecone Burke: Woodland Walk to Lower Vic's"
-          notes="Recycle &rarr; Woodland Walk &rarr Lower Vic's &rarr; wading pools between waterfalls"
-          attributes={['season-summer', 'duration-120', 'terrain-large', 'incline-hilly']} />
-        <HikeTile hikes={hikes} slug='pinecone-burke' 
-          name="Pinecone Burke: Frank's & the Gravel Road Climb to the View"
-          notes="Frank's &rarr; Gravel Road Climb"
-          attributes={['season-any', 'duration-120', 'terrain-large', 'incline-hilly']} />
-        <HikeTile hikes={hikes} slug='jug-island'
-          attributes={['season-any', 'duration-120', 'terrain-large', 'incline-hilly']} />
+        <div class='trails'>
+          <HikeTile hikes={hikes} slug='galette-ave-coquitlam-river'
+            attributes={['weather-any', 'duration-30', 'terrain-small', 'incline-flat']} />
+          <HikeTile hikes={hikes} slug='rocky-point-pier' 
+            attributes={['weather-any', 'duration-30', 'terrain-smooth', 'incline-flat']} />
+          <HikeTile hikes={hikes} slug='minnekhada' notes='Fern Trail, Lodge Trail&comma; or Addington Lookout Trail'
+            attributes={['weather-any', 'duration-30', 'terrain-small', 'incline-ups-and-downs']} />
+          <HikeTile hikes={hikes} slug='harper-park' 
+            attributes={['weather-any', 'duration-30', 'terrain-smooth', 'incline-hilly']} />
+          <HikeTile hikes={hikes} slug='pitt-river' notes="for a loop, try the Deboville Slough. If it's quite windy, consider Colony Farm Regional Park instead."
+            attributes={['weather-any', 'duration-60', 'terrain-smooth', 'incline-flat']} />
+          <HikeTile hikes={hikes} slug='admiralty-point' 
+            notes="option to turn back at Cod Rock or Maple Beach"
+            attributes={['weather-any', 'duration-60', 'terrain-large', 'incline-ups-and-downs']} />
+          <HikeTile hikes={hikes} slug='deiner-creek'
+            attributes={['weather-summer', 'duration-60', 'terrain-large', 'incline-hilly']} />
+          <HikeTile hikes={hikes} slug='pinecone-burke' 
+            name="Pinecone Burke: Frank's to Hustler via Conifer"
+            notes="Frank's &rarr; Conifer Drive &rarr; Hustler"
+            attributes={['weather-any', 'duration-60', 'terrain-large', 'incline-hilly']} />
+          <HikeTile hikes={hikes} slug='pinecone-burke' 
+            name="Pinecone Burke: Woodland Walk to Lower Vic's"
+            notes="Recycle &rarr; Woodland Walk &rarr Lower Vic's &rarr; wading pools between waterfalls"
+            attributes={['weather-summer', 'duration-120', 'terrain-large', 'incline-hilly']} />
+          <HikeTile hikes={hikes} slug='pinecone-burke' 
+            name="Pinecone Burke: Frank's & the Gravel Road Climb to the View"
+            notes="Frank's &rarr; Gravel Road Climb"
+            attributes={['weather-any', 'duration-120', 'terrain-large', 'incline-hilly']} />
+          <HikeTile hikes={hikes} slug='jug-island'
+            attributes={['weather-any', 'duration-120', 'terrain-large', 'incline-hilly']} />
+        </div>
       </div>
 
     </div>
